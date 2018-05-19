595faedf5398e5283a3dbda1d929c996b58d0249
schoolchum <parchana10.patil@gmail.com>
2018-05-19 19:46:30 +0530

    Updated Properites file

---
+++ SchoolManagement/src/main/java/com/school/management/domain/Attendance.java
@@ -0,0 +1,12 @@
+package com.school.management.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "ATTENDANCE")
+public class Attendance {
+	
+	
+
+}
---
+++ SchoolManagement/src/main/java/com/school/management/domain/Student.java
@@ -0,0 +1,49 @@
+package com.school.management.domain;
+
+public class Student {
+
+	private long id;
+	
+	private String academicYear;
+	
+	private String firstName;
+	
+	private String middleName;
+	
+	private String lastName;
+	
+	private String dob;
+	
+	private String gender;
+	
+	private String bloodGroup;
+	
+	private String caste;
+	
+	private String religion;
+	
+	private long standard;
+	
+	private String division;
+	
+	private String nationality;
+	
+	private String permanentAddress;
+	
+	private String correspondenceAddress;
+	
+	private String country;
+	
+	private String state;
+	
+	private String city;
+	
+	private String postalCode;
+	
+	private long fatherId;
+	
+	private long motherId;
+	
+	private long guardianId;
+
+}
