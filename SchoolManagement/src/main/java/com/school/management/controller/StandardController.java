831b89766dd427bd532c5cecd16815579980b72b
Yuvraj IN1076515 <gawadey@vmware.com>
2018-05-22 20:15:10 +0530

    Added cross-origin annotation on standard controller.

--- SchoolManagement/src/main/java/com/school/management/controller/StandardController.java
+++ SchoolManagement/src/main/java/com/school/management/controller/StandardController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import com.school.management.service.StandardService;
 
 @RestController
 @RequestMapping (value = UriConstants.STANDARDS)
+@CrossOrigin( origins = {"http://dev.cloudscripts.co.in", "http://localhost:4200"}, maxAge = 4800, allowCredentials = "false")
 public class StandardController {
 	
 	public static final Logger logger = LoggerFactory.getLogger(StandardController.class);
