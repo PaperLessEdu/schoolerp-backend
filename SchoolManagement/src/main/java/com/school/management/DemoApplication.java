9414e41859b1af535ee9d0bd87bdeb00e0c429ca
pavan.pawar <pavan.pawar@veritas.com>
2018-07-02 16:57:10 +0530

    cors enable

--- SchoolManagement/src/main/java/com/school/management/DemoApplication.java
+++ SchoolManagement/src/main/java/com/school/management/DemoApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
@@ -15,5 +19,16 @@ public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://starbk.cloudscripts.co.in",
+						"http://demo.cloudscripts.co.in");
+			}
+		};
 	}
 }
